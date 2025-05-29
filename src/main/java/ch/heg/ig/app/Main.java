3df241a1751d5359f25b7454cd5b8e9518196a42
Hyakume666 <barthoulot12@gmail.com>
2025-05-29 22:32:10 +0200

    setup of javaFX environment

---
+++ src/main/java/ch/heg/ig/app/Main.java
@@ -0,0 +1,15 @@
+package ch.heg.ig.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
